a50e98271c6c52415b55cc3c282c34cacfa4dc2f
Alexander Penaflor <alexanderpenaflor@Alexanders-MacBook.local>
2022-12-20 10:49:47 -0800

    added correct fourier transform plots

--- virtualsandbox/parq/parquetFiles.py
+++ virtualsandbox/parq/parquetFiles.py
@@ -10,6 +10,7 @@ import argparse
 def arg():
     parser = argparse.ArgumentParser()
     parser.add_argument('name', type=str, help='image')
+    # parser.add_argument('--no-plot', type=str, help = 'prevents plotting')
     args = parser.parse_args()
     return args.name
 
@@ -31,12 +32,182 @@ def createImage(path):
     d = {'raw image':imageFormat, 'array image':imageArrayFlat}
     return d
 
-def gaussian(x, std, mean):
-    y = 1/(np.sqrt(2*np.pi*std**2))*np.exp(-(x-mean**2)/(2*std**2))
-    return y
+def gaussian(x, y, stdx, stdy, x0, y0):
+    return np.exp(-((x-x0)**2/(2*stdx**2)+(y -y0)**2/(2*stdy**2)))
 
-def plotFT(image):
-    arim = image.get('array image')
+
+
+def fourierTransform(image):
+    img = image.get('array image')
+    imgFT = fft2(img)
+    FTx = np.fft.fftfreq(img.shape[0], d=10)
+    FTy = np.fft.fftfreq(img.shape[1], d=10)
+    d = {"xFT":np.abs(imgFT).sum(0), "yFT":np.abs(imgFT).sum(1), "freqX":FTx, "freqY":FTy}
+    return d
+
+def plotFT(image, fourierTransform, plot = True):
+    
+    
+
+    h,w = image.get("array image").shape
+    x,y = np.arange(h), np.arange(w)
+    
+    beamx, beamy = np.meshgrid(np.linspace(-h/2, h/2, h),np.linspace(-w/2, w/2, w))
+    
+
+    xFT = fourierTransform.get("xFT")
+    yFT = fourierTransform.get("yFT")
+    normxFT = xFT/np.max(xFT)
+    normyFT = yFT/np.max(yFT)
+    
+    xdata = image.get("array image").sum(0)
+    ydata = image.get("array image").sum(1)
+    # print(xdata.argmax(), ydata.argmax())
+    normxdata = xdata/np.max(xdata)
+    normydata = ydata/np.max(ydata)
+    
+    nl = "\n"
+    # for i, x in enumerate(normxdata):
+    # print(f"x : {type(np.where((normxdata>0.500) & (normxdata<0.501)))}, \nmax : {np.argmax(normxdata)}")
+    m = normxdata.argmax()
+    fwhm = np.where((normxdata>0.500) & (normxdata<0.501))
+    l, u = fwhm[0][0], fwhm[0][1]
+    # max at index 642, half max at index 
+    # print(f"max index : {m}")
+    print(f"half max index : {l}{nl}{u}{nl}distance from max to lower halfmax : {m-(u-l)}")
+    beam = gaussian(beamx, beamy, 180 , 180, 0, 0)
+    print(x)
+    print(y)
+    if plot is not False:
+        _, [[Y, im],[_, X]] = plt.subplots(2,2, figsize = (12,12),gridspec_kw = {'height_ratios':[2,1], 'width_ratios' : [1,2]})
+        # im.imshow(image.get('array image'))
+        im.imshow(beam, cmap="gray")
+        im.set_aspect('auto', share=True)
+        im.grid()
+        im.set_xticklabels([]),im.set_yticklabels([])
+        
+        
+
+        X.plot(x, normxdata, label = 'x plot')
+        X.plot(x, np.fft.fftshift(normxFT), label = "x FT")
+        X.plot(x, beam.sum(0)/np.max(beam.sum(0)), label = "generated gaussian")
+        X.legend()
+        X.grid()
+
+        Y.plot(normydata, y, label = 'y plot')
+        Y.plot(np.fft.fftshift(normyFT), y, label = "y FT")
+        Y.legend()
+        Y.grid()
+
+        _.axis('off')
+        plt.savefig('plot.png', dpi = 300)
+        # plt.show()
+
+
+def make_gaussian(size, sigma):
+    x, y = np.linspace(-size/2, size/2, size), np.linspace(-size/2, size/2, size)
+    x, y = np.meshgrid(x,y)
+
+    z = np.exp(-((x**2)+(y**2))/(2*(sigma**2)))
+    return z
+
+def plot_fourier_transform(image):
+    image = np.sum(image, axis=1)
+    FT = np.fft.fft(image)
+    FT_shift = np.fft.fftshift(FT)
+    FT_mag = np.abs(200*FT_shift)
+    # FT_mag = np.log10(FT_mag)
+    FT_mag = FT_mag/np.max(FT_mag)
+    plt.plot(image/max(image))
+    plt.plot(FT_mag)
+
+
+size = 128
+sigma = 10
+image = make_gaussian(size, sigma)
+
+shiftx = 30
+shifty = 30
+image_shifted = make_gaussian(size,sigma)
+image_shifted[shifty:, shiftx:] = image[:size-shifty,:size-shiftx]
+
+plt.subplot(2,2,1)
+plt.imshow(image, cmap="gray")
+plt.title("original")
+plt.subplot(2,2,2)
+plot_fourier_transform(image)
+plt.title("1D fourier transform")
+plt.subplot(2,2,3)
+plt.imshow(image_shifted, cmap="gray")
+plt.title("shifted")
+plt.subplot(2,2,4)
+plot_fourier_transform(image_shifted)
+plt.title("1d shifted fourier tranform")
+plt.show()
+
+def saveToFrame():
+    pass
+
+def main():
+    pass
+    # file = arg()
+    # im = createImage(file)
+    # FTim = fourierTransform(im)
+    # print(im.get('array image'))
+    # plotFT(im, FTim, plot=True)
+    
+
+if __name__ == '__main__':
+    main()
+
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# L=1
+# N=2048
+
+# #print(shape)
+# x = np.arange(-L,L,2.*L/N)
+
+# y= np.arange(-L,L,2.*L/N)
+
+# xx, yy=np.meshgrid(x,y)
+# #print(xx,yy)
+# #print(np.size(x))
+# kx= np.fft.fftfreq(np.size(x),2.*L/N)
+# ky=np.fft.fftfreq(np.size(y),2.*L/N)
+# kxx,kyy= np.fft.fftshift(np.meshgrid(kx,ky))
+# #print(xx)
+
+# gauss= np.exp(-xx**2+yy**2)
+# #print(gauss)
+
+# plt.figure()
+# plt.pcolormesh(xx,yy, gauss)
+# plt.gca().set_aspect("equal") 
+# plt.colorbar()
+# # plt.show()
+
+
+# FFT=np.fft.fft2(gauss)
+# FFT_Shift = np.fft.fftshift(FFT)
+# FFT_Log= np.log(FFT)
+# #print(FFT)
+
+# # plt.figure()
+# # This one does not work
+# # plt.pcolormesh(kxx,kyy,abs(np.fft.fftshift(FFT_Log)))
+# plt.plot(kxx.sum(0), np.fft.fftshift(FFT_Log.sum(0)))
+# # However, using imshow works fine
+# # plt.imshow(abs(FFT_Log)) # This one works
+# plt.gca().set_aspect("equal") 
+# plt.colorbar()
+# plt.show()
+
+
+"""
+arim = image.get('array image')
     height, width  = arim.shape
     x,y = np.arange(start = np.floor(-height/2), stop = np.floor(height/2)), np.arange(start = np.floor(-width/2), stop = np.floor(width/2))
     
@@ -53,8 +224,8 @@ def plotFT(image):
     maxXFT2 = np.max(ximageFT2)
     maxYFT2 = np.max(yimageFT2)
 
-    xdataFT = np.abs(np.fft.fftshift(np.fft.fft(np.fft.ifftshift(ydata))))
-    ydataFT = np.abs(ifft(ydata))
+    xdataFT = np.abs(np.fft.fft(ydata))
+    ydataFT = np.abs(np.fft.fft(ydata))
     maxXFT = np.max(xdataFT)
     maxYFT = np.max(ydataFT)
 
@@ -64,41 +235,6 @@ def plotFT(image):
     ysigma = np.std(ydata/maxY)
 
 
-    _, [[Y, im],[_, X]] = plt.subplots(2,2, figsize = (12,12),gridspec_kw = {'height_ratios':[2,1], 'width_ratios' : [1,2]})
-    im.imshow(image.get('array image'))
-    im.set_aspect('auto', share=True)
-    im.grid()
-    im.set_xticklabels([]),im.set_yticklabels([])
-   
-    print(np.abs(maxXFT2))
-    
-    X.plot(x, xdata/maxX, label = 'x plot')
-    X.plot(x, np.abs((ximageFT2))/np.abs(maxXFT2), label = 'x Fourier transform')
-    # X.set_xlim([-10,10])
-    # X.plot(x, ximageFT2/maxXFT2, label = 'x 2D Fourier transform')
-    # X.plot(x, np.exp(-x**2)/maxX, label = 'x Gaussian')
-    X.legend()
-    X.grid()
-
-    Y.plot(ydata/maxY, y, label = 'y plot')
-    # Y.plot(ydataFT/maxYFT, y , label = 'y Fourier transform')
-    # Y.plot(yimageFT2/maxYFT2, y,label = 'y 2D Fourier transform')
-    # Y.plot(gaussian(y, ysigma, ymu)/maxY, y, label = 'y Gaussian')
-    Y.legend()
-    Y.grid()
-
-    _.axis('off')
-    plt.savefig('plot.png', dpi = 100)
-    # plt.show()
-
-def saveToFrame():
-    pass
-
-def main():
-    file = arg()
-    im = createImage(file)
-    # print(im.get('array image'))
-    plotFT(im)
-
-if __name__ == '__main__':
-    main()
+    T = 1/800
+    xf = np.linspace(0, 1/(2*T), len(x))
+"""
